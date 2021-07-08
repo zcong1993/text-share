3e4d22fd2fd505aeba87b65c2804e0d514a92f2e
zcong1993 <zhangcong1992@gmail.com>
2021-07-08 15:14:26 +0800

    feat: frontend done

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,105 @@
 import { Component } from 'react'
 import Head from 'next/head'
+import { format } from 'date-fns'
+import Clipboard from 'react-clipboard.js'
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: '',
       data: [
         {
           id: '12345',
           content: 'test text 1',
-          createdAt: '2021-07-08T06:05:16.266Z'
+          createdAt: new Date('2021-07-08T06:05:16.266Z')
         },
         {
           id: '12346',
           content: 'test text 2',
-          createdAt: '2021-07-08T06:05:16.266Z'
+          createdAt: new Date('2021-07-08T06:05:16.266Z')
         },
       ],
     };
   }
 
+  handleTextChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  async handleAdd() {
+    if (!this.state.text.trim()) {
+      return
+    }
+
+    console.log('add ', this.state.text.trim())
+  }
+
+  async handleDelete(id) {
+    console.log('delete ', id)
+  }
+
+  onCopySuccess() {
+    console.info('successfully copied')
+  }
+
+  renderList() {
+    return this.state.data.map(d => {
+      return (
+        <div className="shadow bg-white rounded-lg p-5 mb-5" key={d.id}>
+          <div>
+            {d.content}
+          </div>
+          <div className="mt-5 flex justify-between items-center">
+            <div className="text-gray-500 text-sm">{ format(d.createdAt, 'yyyy-MM-dd HH:mm:ss') }</div>
+            <div className="flex space-x-2">
+                <Clipboard className="bg-green-50 text-green-500 px-2 h-6 text-xs rounded-lg" data-clipboard-text={d.content} onSuccess={this.onCopySuccess.bind(this)}>
+                  Copy
+                </Clipboard>
+              <button className="bg-blue-50 text-blue-500 px-2 h-6 text-xs rounded-lg" onClick={() => this.handleDelete(d.id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
-      <div className="flex flex-col items-center min-h-screen py-2">
+      <div>
         <Head>
           <title>Text Share</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
   
-        <main className="flex flex-col items-center w-full flex-1 px-20 text-center">
-          <h1 className="text-6xl font-bold">
-            Welcome to{' '}
-            <a className="text-blue-600" href="https://nextjs.org">
-              Next.js
-            </a>
-          </h1>
-  
-          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-            <div
-              className="p-6 mt-6 text-left border w-96 md:w-full rounded-xl"
-            >
-              <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Find in-depth information about Next.js features and API.
-              </p>
+        <main>
+          <div className="bg-gray-50 min-h-screen">
+            <div className="max-w-screen-md mx-auto py-10">
+              <div className="shadow bg-white rounded-lg p-5 space-y-5 mb-5">
+                <div className="text-xl font-medium">Share Text</div>
+                <div className="w-full">
+                  <textarea
+                    className="w-full block border bg-transparent rounded-lg resize-none p-2 focus:outline-none focus:border-blue-500 focus:ring"
+                    rows="10"
+                    value={this.state.text}
+                    onChange={this.handleTextChange.bind(this)}
+                  ></textarea>
+                </div>
+                <div className="flex justify-end items-end">
+                  <button onClick={this.handleAdd.bind(this)} className="bg-green-500 text-white px-4 h-10 inline-flex items-center rounded-lg">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            
+              {this.renderList()}
+
             </div>
-  
-            <div
-              className="p-6 mt-6 text-left border w-96 md:w-full rounded-xl"
-            >
-              <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Learn about Next.js in an interactive course with quizzes!
-              </p>
-            </div>
-          </div>
+          </div>;
         </main>
       </div>
     )
